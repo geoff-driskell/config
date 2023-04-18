f4179737a0a4aff701093f2fdc2a31a66520f111
Geoff Driskell <geoff.driskell@gmail.com>
2023-04-18 08:10:25 -0400

    stable naptime state

--- appdaemon/apps/naptime.py
+++ appdaemon/apps/naptime.py
@@ -10,13 +10,7 @@ class Naptime(hass.Hass):
         """Initialize naptime."""
         # pylint: disable=attribute-defined-outside-init
         self.avery_asleep = self.get_entity("input_boolean.avery_naptime")
-        self.avery_asleep_timer = None
-        self.avery_asleep_datetime = self.get_entity("input_datetime.avery_asleep_since")
-        self.avery_sleep_duration = None
         self.benjamin_asleep = self.get_entity("input_boolean.benjamin_naptime")
-        self.benjamin_asleep_timer = None
-        self.benjamin_asleep_datetime = self.get_entity("input_datetime.benjamin_asleep_since")
-        self.benjamin_sleep_duration = None
 
         self.naptime = self.get_entity("binary_sensor.twins_sleeping")
 
@@ -27,6 +21,7 @@ class Naptime(hass.Hass):
         self.avery_asleep.listen_state(self.naptime_started, new = "on")
         self.avery_asleep.listen_state(self.avery_awake, new = "off")
         self.benjamin_asleep.listen_state(self.benjamin_awake, new = "off")
+        self.listen_state(self.song_stopped, "media_player.averys_room")
 
     def naptime_started(self, entity, attribute, old, new, kwargs):
         """Start the twins naptime routine."""
@@ -39,13 +34,13 @@ class Naptime(hass.Hass):
         if self.now_is_between("08:00:00", "17:00:00"):
             # this is a nap, set the helper for a nap
             self.turn_on("input_boolean.twins_taking_a_nap")
+            self.set_state("sensor.avery_last_nap_start", state=self.get_now(), attributes={"icon":"mdi:clock", "friendly_name":"Avery Last Nap", "device_class":"timestamp", "unique_id":"cbc0ec4f-652e-47f0-a698-670d6111d8cd"})
+            self.set_state("sensor.benjamin_last_nap_start", state=self.get_now(), attributes={"icon":"mdi:clock", "friendly_name":"Benjamin Last Nap", "device_class":"timestamp", "unique_id":"2fa44660-c261-4d73-a0c2-f4868c8885fc"})
         else:
             # This is bedtime, set the helper for bedtime
             self.turn_on("input_boolean.twins_down_for_the_night")
-
-        # Record Starting Sleep Time
-        self.avery_asleep_datetime.call_service("set_datetime", timestamp=self.get_now_ts())
-        self.benjamin_asleep_datetime.call_service("set_datetime", timestamp=self.get_now_ts())
+            self.set_state("sensor.avery_last_bed_start", state=self.get_now(), attributes={"icon":"mdi:clock", "friendly_name":"Avery Last Bed", "device_class":"timestamp", "unique_id":"944b332b-7bc4-40ad-815b-675dd1ad8ba6"})
+            self.set_state("sensor.benjamin_last_bed_start", state=self.get_now(), attributes={"icon":"mdi:clock", "friendly_name":"Benjamin Last Bed", "device_class":"timestamp", "unique_id":"3e32011b-bca5-408e-b767-9b1a9d67813a"})
 
         # Kill the lights
         self.turn_off("light.nursery_ceiling_fan_light")
@@ -54,6 +49,8 @@ class Naptime(hass.Hass):
         self.turn_off("switch.attic_main_lights")
         self.turn_off("switch.stairs_lights")
         self.turn_off("light.upstairs_hallway_lights")
+        self.turn_off("light.loft_lamp")
+        self.turn_off("light.loft_lights")
 
         # Start the fans
         self.turn_on("fan.nursery_ceiling_fan", percentage = "75")
@@ -62,6 +59,7 @@ class Naptime(hass.Hass):
         # Turn on Benjamin's sound machine
         self.turn_on("switch.twins_sound_machine")
         
+        self.set_state("binary_sensor.play_against_the_wind", state="on", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
     
     def avery_awake(self, entity, attribute, old, new, kwargs):
         """Handle Avery waking up."""
@@ -74,6 +72,7 @@ class Naptime(hass.Hass):
                 self.log("Benjamin is already awake. Turning off the bedtime helper.", level="INFO")
                 self.turn_off("input_boolean.twins_down_for_the_night")
                 self.turn_off("input_boolean.twins_asleep")
+                self.set_state("binary_sensor.play_against_the_wind", state="off", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
         else:
             self.log("Avery woke up from her nap.", level="INFO")
             if self.get_state("input_boolean.benjamin_naptime") == "on":
@@ -82,6 +81,7 @@ class Naptime(hass.Hass):
                 self.log("Benjamin is already awake. Turning off the naptime helper.", level="INFO")
                 self.turn_off("input_boolean.twins_taking_a_nap")
                 self.turn_off("input_boolean.twins_asleep")
+                self.set_state("binary_sensor.play_against_the_wind", state="off", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
         
         # Turn off her fan
         self.turn_off("fan.averys_bedroom_ceiling_fan")
@@ -104,6 +104,7 @@ class Naptime(hass.Hass):
                 self.log("Avery is already awake. Turning off the bedtime helper.", level="INFO")
                 self.turn_off("input_boolean.twins_down_for_the_night")
                 self.turn_off("input_boolean.twins_asleep")
+                self.set_state("binary_sensor.play_against_the_wind", state="off", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
         else:
             self.log("Benjamin woke up from his nap.", level="INFO")
             if self.get_state("input_boolean.avery_naptime") == "on":
@@ -112,6 +113,7 @@ class Naptime(hass.Hass):
                 self.log("Avery is already awake. Turning off the naptime helper.", level="INFO")
                 self.turn_off("input_boolean.twins_taking_a_nap")
                 self.turn_off("input_boolean.twins_asleep")
+                self.set_state("binary_sensor.play_against_the_wind", state="off", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
 
         # Turn off his fan
         self.turn_off("fan.nursery_ceiling_fan")
@@ -125,3 +127,12 @@ class Naptime(hass.Hass):
         else:
             self.log("Turning benjamin's light on.")
             self.turn_on("light.nursery_ceiling_fan_light", brightness_pct = "15")
+
+    def song_stopped(self, entity, attribute, old, new, kwargs):
+        if self.get_state("media_player.averys_room") == "paused" or self.get_state("media_player.averys_room") == "idle":
+            if self.get_state("binary_sensor.twins_sleeping") == "on":
+                self.set_state("binary_sensor.play_against_the_wind", state="off", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
+                self.run_in(self.restart_song, 1)
+
+    def restart_song(self, kwargs):
+        self.set_state("binary_sensor.play_against_the_wind", state="on", attributes={"icon":"mdi:music", "friendly_name":"Play Against The Wind", "unique_id":"f6f40b67-264f-48e9-ad79-d42035761b23"})
