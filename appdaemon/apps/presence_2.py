e50769674b943f61bbbea06ffbb2e7800dc24aed
Geoff Driskell <geoff.driskell@gmail.com>
2023-05-07 13:50:01 -0400

    formatting updates

--- appdaemon/apps/presence_2.py
+++ appdaemon/apps/presence_2.py
@@ -7,6 +7,7 @@ import utils
 
 __version__ = "0.0.1"
 
+
 class State(Enum):
     Home = "home"
     JustArrived = "just_arrived"
@@ -18,18 +19,19 @@ class State(Enum):
     def from_string(label):
         if not isinstance(label, str):
             raise TypeError("Argument label needs to be a string.")
-        if label.casefold() in ('home'):
+        if label.casefold() in ("home"):
             return State.Home
-        elif label.casefold() in ('arrived', 'just arrived', 'just_arrived'):
+        elif label.casefold() in ("arrived", "just arrived", "just_arrived"):
             return State.JustArrived
-        elif label.casefold() in ('away', 'not_home', 'not home'):
+        elif label.casefold() in ("away", "not_home", "not home"):
             return State.Away
-        elif label.casefold() in ('left', 'just left', 'just_left'):
+        elif label.casefold() in ("left", "just left", "just_left"):
             return State.JustLeft
-        elif label.casefold() in ('extended away', 'extended_away', 'vacation'):
+        elif label.casefold() in ("extended away", "extended_away", "vacation"):
             return State.ExtendedAway
         else:
             raise ValueError(f"Argument label '{label}' is not a valid state.")
+
 
 class TrackerState(Enum):
     Home = "home"
@@ -44,24 +46,37 @@ class TrackerState(Enum):
         else:
             return TrackerState.NotHome
 
+
 class Validator:
     from voluptuous import Schema, Required, Optional, Range, All, Or
 
     TIME_5_MINUTES = 5 * 60
     TIME_24_HOURS = 24 * 60 * 60
 
-    SCHEMA = Schema({
-        Required('tracker'): str,
-        Required('state'): str,
-        Optional('map', default={state.value: state.value for state in State}): {state.value: str for state in State},
-        Optional('just_left_delay', default=TIME_5_MINUTES): utils.parse_duration_literal,
-        Optional('just_arrived_delay', default=TIME_5_MINUTES): utils.parse_duration_literal,
-        Optional('extended_away_delay', default=TIME_24_HOURS): utils.parse_duration_literal
-    }, extra=True)
+    SCHEMA = Schema(
+        {
+            Required("tracker"): str,
+            Required("state"): str,
+            Optional("map", default={state.value: state.value for state in State}): {
+                state.value: str for state in State
+            },
+            Optional(
+                "just_left_delay", default=TIME_5_MINUTES
+            ): utils.parse_duration_literal,
+            Optional(
+                "just_arrived_delay", default=TIME_5_MINUTES
+            ): utils.parse_duration_literal,
+            Optional(
+                "extended_away_delay", default=TIME_24_HOURS
+            ): utils.parse_duration_literal,
+        },
+        extra=True,
+    )
 
     @classmethod
     def validate(cls, dict):
         return cls.SCHEMA(dict)
+
 
 class Presence(hass.Hass):
     def initialize(self):
@@ -69,13 +84,13 @@ class Presence(hass.Hass):
 
         config = Validator.validate(self.args)
 
-        self._tracker_entity = config['tracker']
-        self._state_entity = config['state']
-        self._just_left_delay = config['just_left_delay']
-        self._just_arrived_delay = config['just_arrived_delay']
-        self._extended_away_delay = config['extended_away_delay']
+        self._tracker_entity = config["tracker"]
+        self._state_entity = config["state"]
+        self._just_left_delay = config["just_left_delay"]
+        self._just_arrived_delay = config["just_arrived_delay"]
+        self._extended_away_delay = config["extended_away_delay"]
 
-        user_map = config['map']
+        user_map = config["map"]
         user_map = {State.from_string(k): v for k, v in user_map.items()}
         self._map = {state: user_map.get(state, state.value) for state in State}
         self._imap = {v: k for k, v in self._map.items()}
@@ -83,7 +98,7 @@ class Presence(hass.Hass):
         self.timer = None
         self.current_state = None
 
-        if config.get('init_options', False):
+        if config.get("init_options", False):
             self._set_options()
             time.sleep(1)
 
@@ -101,17 +116,19 @@ class Presence(hass.Hass):
                 self._set_person_state(State.JustArrived)
         if old_state == TrackerState.Home and new_state == TrackerState.NotHome:
             self._set_person_state(State.JustLeft)
-    
+
     def _on_scheduled_state_change(self, kwargs):
-        new_state = kwargs.pop('new_state')
+        new_state = kwargs.pop("new_state")
         self._set_person_state(new_state)
-    
+
     def _schedule_state_change(self, delay, new_state):
-        self.timer = self.run_in(self._on_scheduled_state_change, delay=delay, new_state=new_state)
+        self.timer = self.run_in(
+            self._on_scheduled_state_change, delay=delay, new_state=new_state
+        )
 
     def _set_person_state(self, state):
         self._current_state = state
-        
+
         if self.timer:
             self.cancel_timer(self.timer)
         if state == State.JustArrived:
@@ -127,14 +144,27 @@ class Presence(hass.Hass):
         if current_state is None:
             self.log(f"Current state in hass is invalid. Falling back to {State.Home}.")
             current_state = State.Home
-        tracker_state = TrackerState.from_string(self.get_state(entity_id=self._tracker_entity))
-        if tracker_state is TrackerState.Home and current_state not in (State.JustArrived, State.Home):
-            self.log(f"Current state '{current_state}' is invalid with tracker state '{tracker_state}'. Resetting to home.")
+        tracker_state = TrackerState.from_string(
+            self.get_state(entity_id=self._tracker_entity)
+        )
+        if tracker_state is TrackerState.Home and current_state not in (
+            State.JustArrived,
+            State.Home,
+        ):
+            self.log(
+                f"Current state '{current_state}' is invalid with tracker state '{tracker_state}'. Resetting to home."
+            )
             current_state = State.Home
-        elif tracker_state is TrackerState.NotHome and current_state not in (State.JustLeft, State.Away, State.ExtendedAway):
-            self.log(f"Current state '{current_state}' is invalid with tracker state '{tracker_state}'. Resetting to away.")
+        elif tracker_state is TrackerState.NotHome and current_state not in (
+            State.JustLeft,
+            State.Away,
+            State.ExtendedAway,
+        ):
+            self.log(
+                f"Current state '{current_state}' is invalid with tracker state '{tracker_state}'. Resetting to away."
+            )
             current_state = State.Away
-        
+
         self._set_person_state(state=current_state)
 
     def _set_options(self):
@@ -143,9 +173,7 @@ class Presence(hass.Hass):
         options = list(self._map.values())
         self.log(f"Setting state options to {options}.")
         self.call_service(
-            "input_select/set_options",
-            entity_id=self._state_entity,
-            options=options
+            "input_select/set_options", entity_id=self._state_entity, options=options
         )
 
         if current_state not in options:
